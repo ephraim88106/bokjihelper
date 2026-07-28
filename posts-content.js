fd932b915634a758a35257b267a8af479acca932
ephraim88106 <namho1123@gmail.com>
2026-07-29 03:01:55 +0900

    feat: 2026-07-29 복지뉴스 2건 (긴급복지지원 199만원·중장년 재취업 인센티브 360만원)

--- posts-content.js
+++ posts-content.js
@@ -1,5 +1,7 @@
 // Auto-generated. News article bodies, loaded only by article.html.
 export const newsContent = {
+  221: "<iframe src=\"archive/2026-07-29-2-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
+  220: "<iframe src=\"archive/2026-07-29-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
   219: "<iframe src=\"archive/2026-07-27-2-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
   218: "<iframe src=\"archive/2026-07-27-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
   217: "<iframe src=\"archive/2026-07-26-2-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
