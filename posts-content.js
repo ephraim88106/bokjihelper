3494e75a7d48df067d6d692e3efafdbfe050b2c2
ephraim88106 <namho1123@gmail.com>
2026-08-13 01:19:52 +0000

    복지 제도 안내 발행 (2026-08-13): 장애인연금과 장애수당의 대상·금액 차이와 중복 수급 불가 정리

--- posts-content.js
+++ posts-content.js
@@ -1,5 +1,6 @@
 // Auto-generated. News article bodies, loaded only by article.html.
 export const newsContent = {
+  247: "<iframe src=\"archive/2026-08-13-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
   246: "<iframe src=\"archive/2026-08-12-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
   245: "<iframe src=\"archive/2026-08-11-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
   244: "<iframe src=\"archive/2026-08-10-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
