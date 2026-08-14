60cb282b6d534c0d18187c5bcc7bede62d14ee3b
ephraim88106 <namho1123@gmail.com>
2026-08-14 01:12:28 +0000

    복지 제도 안내 발행 (2026-08-14): 긴급복지지원제도의 위기상황 인정 사유와 2026년 가구원수별 생계지원 금액·소득재산 기준 정리

--- posts-content.js
+++ posts-content.js
@@ -1,5 +1,6 @@
 // Auto-generated. News article bodies, loaded only by article.html.
 export const newsContent = {
+  248: "<iframe src=\"archive/2026-08-14-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
   247: "<iframe src=\"archive/2026-08-13-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
   246: "<iframe src=\"archive/2026-08-12-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
   245: "<iframe src=\"archive/2026-08-11-1-복지뉴스.html\" style=\"width:100%;border:none;\" scrolling=\"no\" onload=\"this.style.height=this.contentWindow.document.documentElement.scrollHeight+'px'\" loading=\"lazy\" title=\"복지뉴스\"></iframe>",
